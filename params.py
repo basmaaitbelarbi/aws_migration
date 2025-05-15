5af16d19a7c95b8b2eb2eb088223bbe541c1e97e
basma boussi <basmaaitbelarbi@gmail.com>
2025-05-15 19:03:43 +0200

    changed file params.py

--- params.py
+++ params.py
@@ -1,5 +1,12 @@
+# USER = "postgres"
+# PASSWORD = "Postgres26"
+# RDS_ENDPOINT = "postgresdb.ctousssecaxr.eu-north-1.rds.amazonaws.com"
+# DB_NAME = "retailDB" # 'postgres'
+# PORT = "5432"
+
+
 USER = "postgres"
-PASSWORD = "Postgres26"
-RDS_ENDPOINT = "postgresdb.ctousssecaxr.eu-north-1.rds.amazonaws.com"
-DB_NAME = "retailDB" # 'postgres'
+PASSWORD = "Basma1230"
+RDS_ENDPOINT = "database-1.crwoc04w8j2z.eu-north-1.rds.amazonaws.com"
+DB_NAME = "retail_DB" # 'postgres'
 PORT = "5432"
